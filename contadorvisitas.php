72d29089f97dbbd838af392199ee232b159ea95d
Belén <32224724+SmoothBrave@users.noreply.github.com>
2017-10-05 11:43:57 +0200

    Add files via upload

---
+++ contadorvisitas.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+if(isset($_COOKIE['contador'])){
+	setcookie('contador',$_COOKIE['contador']+1,time()+365*24*60*60);
+	echo " Número de visitas:" .$_COOKIE['contador'];
+}else{
+	setcookie('contador',1,time()+365*24*60*60);
+	echo " Bienvenido por primera vez a nuestra página";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
